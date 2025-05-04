bd30c910c11b934775d0096a50896726dc8c283f
Kochengku <159976644+Kochengku@users.noreply.github.com>
2025-05-04 15:45:54 +0700

    Add files via upload

--- plugins/group/tes.js
+++ plugins/group/tes.js
@@ -1,1 +1,11 @@
+async function handler(m, { conn, usedPrefix, text, command, isOwner, isPrivate, usedPrefix:_p, __dirname, args }) {
+switch (command) {
+case 'tes': {
+m.reply('Okew')
+}
+}
 
+handler.command = ['tes']
+handler.help = ['tes']
+handler.tags = ['akun']
+export default handler
